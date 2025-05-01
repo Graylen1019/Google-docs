03d4a3cb703812cf5e03ab53bf35f6ef32cd9f3a
Graylen <delilhabigelow@gmail.com>
2025-05-01 03:40:46 -0400

    Added Notifications feature

--- src/app/documents/[documentId]/actions.ts
+++ src/app/documents/[documentId]/actions.ts
@@ -1,25 +1,34 @@
 "use server";
 
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import { ConvexHttpClient } from "convex/browser";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { api } from "../../../../convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export async function getDocuments(ids: Id<"documents">[]) {
+  return await convex.query(api.documents.getByIds, { ids });
+}
 
 interface PostClaim {
-    id: string;
-  }
-  
+  id: string;
+}
 
 export async function getUsers() {
-    const { sessionClaims } = await auth()
-    const clerk = await clerkClient();
+  const { sessionClaims } = await auth();
+  const clerk = await clerkClient();
 
-    const response = await clerk.users.getUserList({
-        organizationId: [(sessionClaims?.o as PostClaim).id]
-    });
+  const response = await clerk.users.getUserList({
+    organizationId: [(sessionClaims?.o as PostClaim).id],
+  });
 
-    const users = response.data.map((user) => ({
-        id: user.id,
-        name: user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous",
-        avatar: user.imageUrl,
-    }));
+  const users = response.data.map((user) => ({
+    id: user.id,
+    name:
+      user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous",
+    avatar: user.imageUrl,
+  }));
 
-    return users;
+  return users;
 }
